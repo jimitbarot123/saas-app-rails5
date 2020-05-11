8a859358a50fe1ef1dd307c4642f1361577a7b98
Jimit Barot <jimitbarot31@gmail.com>
2020-05-11 19:20:13 +0530

    Added devise, milia and bootstrap gems and updated related changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_tenant!
+
+  #rescue_from ::Milia::Control::MaxTenantExceeded, :with [:max_tenants]
+  #rescue_from ::Milia::Control::InvalidTenantAccess, :with [:invalid_tenant]
 end
